c7ee24e508c1af66c53a88e11eb131a2274b130a
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-09-14 11:10:23 +0900

    make restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+Restaurant.destroy_all
+
+puts "making 10 restaurants"
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    rating: rand(1..10)
+  )
+  restaurant.save
+end
+puts "finish!"
